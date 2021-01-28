d768ed3a2299dbfa2e209e8e578c0898944dcd4c
Sviatoslav <58343993+svyat2003@users.noreply.github.com>
2021-01-28 19:18:41 +0200

    Nothing much
    Yes

---
+++ Source.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+
+struct Vector3D {
+	int x;
+	int y;
+	int z;
+};
+
+
+struct Vector2D {
+	int x;
+	int y;
+};
+
+
+class World {
+private:
+	char*** a;
+	short size;
+public:
+	World(short n) {
+		CreateWorld(n);
+	}
+
+	World() {
+		NULL;
+	}
+
+
+	void CreateWorld(short n) {
+		size = n;
+		a = new char** [n];
+		for (int x = 0; x < n; x++) {
+			a[x] = new char* [n];
+			for (int y = 0; y < n; y++) {
+				a[x][y] = new char[n];
+				for (int z = 0; z < n; z++) {
+					a[x][y][z] = 48;
+				}
+			}
+		}
+	}
+
+
+	void Display(int raw) {				// raw - z
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				cout << a[x][y][raw] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+
+	char& GetPos(int x, int y, int z) {
+		return a[x][y][z];
+	}
+
+
+	char& GetPos(Vector3D point) {
+		return GetPos(point.x, point.y, point.z);
+	}
+
+
+	~World() {
+		for (int x = 0;x<size;x++){
+			for (int y = 0;y<size;y++){
+				delete [] a[x][y];
+			}
+			delete [] a[x];
+		}
+		delete [] a;
+    }
+
+};
+
+
+int main()
+{
+	World world; World world2(7);
+
+	world.CreateWorld(50);
+	world.GetPos(2, 2, 1) = 81;
+	world.Display(1);
+	while (true);
+}
