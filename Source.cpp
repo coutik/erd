2e21bcaa20b621f3087d19ced1a5dcbc810d495c
Sviatoslav <58343993+svyat2003@users.noreply.github.com>
2021-02-04 10:10:40 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+
+
+void clearScreen() {
+#ifdef _WIN32
+	COORD topLeft = { 0, 0 };
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	DWORD written;
+
+	GetConsoleScreenBufferInfo(console, &screen);
+	FillConsoleOutputCharacterA(
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	FillConsoleOutputAttribute(
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	SetConsoleCursorPosition(console, topLeft);
+#endif
+#ifndef _WIN32
+	cout << "\033[2J\033[1;1H";
+#endif
+}
 
 
 struct Vector3D {
@@ -44,7 +74,7 @@ public:
 	}
 
 
-	void Display(int raw) {				// raw - z
+	void Display (int raw) {				// raw - z
 		for (int x = 0; x < size; x++) {
 			for (int y = 0; y < size; y++) {
 				cout << a[x][y][raw] << " ";
@@ -54,25 +84,25 @@ public:
 	}
 
 
-	char& GetPos(int x, int y, int z) {
+	char& GetPos (int x, int y, int z) {
 		return a[x][y][z];
 	}
 
 
-	char& GetPos(Vector3D point) {
+	char& GetPos (Vector3D point) {
 		return GetPos(point.x, point.y, point.z);
 	}
 
 
 	~World() {
-		for (int x = 0;x<size;x++){
-			for (int y = 0;y<size;y++){
-				delete [] a[x][y];
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				delete[] a[x][y];
 			}
-			delete [] a[x];
+			delete[] a[x];
 		}
-		delete [] a;
-    }
+		delete[] a;
+	}
 
 };
 
@@ -82,7 +112,12 @@ int main()
 	World world; World world2(7);
 
 	world.CreateWorld(50);
-	world.GetPos(2, 2, 1) = 81;
+	world.GetPos(2, 2, 1) = 70;
 	world.Display(1);
+
+	auto extended = _getch();
+	if (extended == 48) {
+		clearScreen();
+	}
 	while (true);
 }
